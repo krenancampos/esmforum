72950d63dc1e5d2b6ed2cca8baf872c0f1384188
Kayke Campos <kaykerenan@hotmail.com>
2022-12-01 23:22:00 -0300

    Implementação de teste de cadastro de resposta

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,15 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de resposta', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  const perguntas = modelo.listar_perguntas();
+  id = modelo.get_pergunta(perguntas[0].id_pergunta);
+  modelo.cadastrar_resposta(id.id_pergunta, "2");
+  
+  const respostas = modelo.get_respostas(perguntas[0].id_pergunta)
+
+  expect(respostas.length).toBe(1);
+  expect(respostas[0].texto).toBe('2');
+});
